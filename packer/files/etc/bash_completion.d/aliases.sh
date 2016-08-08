13d5010309cafb233de2874ba0c4b3ea5d18fc40
Johannes 'fish' Ziemke <github@freigeist.org>
2016-08-08 12:30:22 +0200

    Fix aliases

--- packer/files/etc/bash_completion.d/aliases.sh
+++ packer/files/etc/bash_completion.d/aliases.sh
@@ -4,6 +4,4 @@ torus_etcd_flags="-C https://$(hostname):2379 --etcd-ca-file /etc/ssl/5pi-ca.pem
 alias torusctl="torusctl $torus_etcd_flags"
 alias torusblk="torusblk $torus_etcd_flags"
 
-
-alias etcdctl="etcdctl --endpoints https://$(hostname):2379 --ca-file /etc/ssl/5pi-ca.pem --cert-file /etc/ssl/server.pem --key-file /etc/ssl/server-key.pem cluster-health"
-
+alias etcdctl="etcdctl --endpoints https://$(hostname):2379 --ca-file /etc/ssl/5pi-ca.pem --cert-file /etc/ssl/server.pem --key-file /etc/ssl/server-key.pem"
