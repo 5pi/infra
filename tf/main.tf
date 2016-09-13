9e89ddc926f092e3238570dfae7630186916ccb1
Johannes 'fish' Ziemke <github@freigeist.org>
2016-09-13 11:00:17 +0200

    Make tf create instances before deleting them

--- tf/main.tf
+++ tf/main.tf
@@ -25,7 +25,7 @@ variable "ip_int_prefix" {
 
 variable "image" {
   type    = "string"
-  default = "19162982"
+  default = "19641332"
 }
 
 provider "digitalocean" {
@@ -53,6 +53,9 @@ resource "digitalocean_droplet" "master" {
   private_networking = true
   ipv6               = true
   ssh_keys           = ["${digitalocean_ssh_key.default.id}"]
+  lifecycle {
+    create_before_destroy = true
+  }
 
   provisioner "file" {
     source      = "configure.sh"
